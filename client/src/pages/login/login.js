1ea644fe04384ccbe96df9e185a40bb9b6745321
jewel109 <jewelrana176517@gmail.com>
2023-04-27 13:07:36 +0600

    login form is completed with chakra

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,25 +1,97 @@
-import react, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useLogin } from '../../hooks/useLogin.js'
-
+import react, { useEffect, useState } from "react";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+} from "@chakra-ui/react";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../hooks/useLogin.js";
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required").nullable(),
+  password: Yup.string()
+    .min(4, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required").nullable(),
+});
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const { login, error } = useLogin()
-  const navigate = useNavigate()
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await login(email, password)
+  const { login } = useLogin();
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
 
-
-    const err = await login(email, password)
-    if (err == null) {
-      navigate("/profile")
-    }
-  }
-  // TODO: make fully chakra based 
+  // TODO: make fully chakra based
   return (
-  <p> hello</p>
-  )
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={async (values, actions) => {
+        const err = await login(values.email, values.password);
+        console.log(err);
+        if (err == null) {
+          navigate("/profile");
+        } else {
+          setError(err);
+          values.email = "";
+          values.password = "";
+        }
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Field name="email">
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.email && form.touched.email}>
+                <FormLabel>Email</FormLabel>
+                <Input {...field} placeholder="email" />
+                <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="password">
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.password && form.touched.password}
+              >
+                <FormLabel>Password</FormLabel>
+                <Input {...field} placeholder="password" />
+                <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Login
+          </Button>
+          {err
+            ? (
+              <Alert status="error" mt={2} borderRadius="base">
+                <AlertIcon w={9} h={8} />
+                <AlertTitle>{err}</AlertTitle>
+                <AlertDescription>
+                  Try again
+                </AlertDescription>
+              </Alert>
+            )
+            : null}
+        </Form>
+      )}
+    </Formik>
+  );
 }
