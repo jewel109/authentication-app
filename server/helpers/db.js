200552dfdd3d9d25630de256af9e93adfc06df4f
jewel109 <jewelrana176517@gmail.com>
2023-05-01 18:03:08 +0600

    changed the async return of db

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -4,11 +4,11 @@ async function connectingDB() {
   try {
     const { NODE_ENV } = process.env;
     if (NODE_ENV === "production") {
-      mongoose.connect(
+      await mongoose.connect(
         "mongodb+srv://jewelrana176517:1234abu@cluster0.nobr86s.mongodb.net/?retryWrites=true&w=majority",
       );
     } else {
-      mongoose.connect("mongodb://localhost:27017/mern-auth");
+      await mongoose.connect("mongodb://localhost:27017/mern-auth");
     }
     console.log("db running");
   } catch (err) {
