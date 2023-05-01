dc044c731560374891b13b559897fcafbd217874
jewel109 <jewelrana176517@gmail.com>
2023-05-01 17:18:08 +0600

    base url for axios is changed

--- client/src/services/axios/index.js
+++ client/src/services/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://authentication-app-eqi1.onrender.com"
 })
 instance.defaults.headers.post['Content-Type'] = 'application/json'
 
